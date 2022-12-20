ed33d4ba5161f0124d49aea341dfb58cda178207
BENSAMEH <121119973+BENSAMEH@users.noreply.github.com>
2022-12-20 20:34:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace prime_number_task_discrete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTER THE FIRST NUMBER");
+            int firstnumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("ENTER THE SECOND NUMBER");
+            int secondnumber = int.Parse(Console.ReadLine());
+
+            for (int i = firstnumber; i <= secondnumber; i++)
+            {
+                if (i == 1) { continue; }
+                bool isprime = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0  )
+
+
+                    { 
+                        isprime = false;
+                        break;
+
+
+                    }
+                    
+                        
+                    
+                    else
+                    {
+                        isprime = true;
+
+                    }
+
+
+                }
+                if (isprime == true)
+                {
+                    Console.WriteLine("THE NUMBER {0} IS PRIME", i);
+                }
+
+
+
+            }
+
+
+
+        }
+    }
+}
+
